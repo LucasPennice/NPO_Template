6962f8ff8f5e9623eebc47c1e9c964bcbc91bd03
Lucas <lucaspennice@gmail.com>
2022-01-01 22:07:29 -0300

    fixed style on credits page

--- pages/creditos.js
+++ pages/creditos.js
@@ -1,15 +1,68 @@
 const creditos = () => {
 	return (
-		<div>
-			<h2>
-				Diseño y desarrollo: Lucas Pennice (<a href="">Portfolio</a>{' '}
-				Email:lucaspennice@gmail.com Tel: +5493413584946 <a href="">GitHub</a>)
-			</h2>
-			<h2>
-				Ilustraciones cortesía de:{' '}
-				<a href="https://storyset.com/">StorySet.com</a> y{' '}
-				<a href="https://www.freepikcompany.com/">FreePikCompany</a>
-			</h2>
+		<div className="creditos_container">
+			<div>
+				<h1>
+					Diseño y desarrollo: <span>Lucas Pennice</span>
+				</h1>
+				<h2>
+					Portfolio:{' '}
+					<a
+						href="https://www.lucaspennice.ar/"
+						target="_blank"
+						rel="noreferral"
+					>
+						lucaspennice.ar
+					</a>
+				</h2>
+				<h2>
+					Email:{' '}
+					<a
+						href="mailto:lucaspennice@gmail.com"
+						target="_blank"
+						rel="noreferral"
+					>
+						lucaspennice@gmail.com
+					</a>
+				</h2>
+				<h2>
+					Teléfono:{' '}
+					<a
+						href="https://wa.me/+5493413584946"
+						target="_blank"
+						rel="noreferral"
+					>
+						+5493413584946
+					</a>
+				</h2>
+				<h2>
+					Github:{' '}
+					<a
+						href="https://github.com/LucasPennice"
+						target="_blank"
+						rel="noreferral"
+					>
+						github.com/LucasPennice
+					</a>
+				</h2>
+				<h1>
+					Ilustraciones cortesía de:{' '}
+					<span>
+						<a href="https://storyset.com/" target="_blank" rel="noreferral">
+							StorySet.com
+						</a>{' '}
+						y{' '}
+						<a
+							href="https://www.freepikcompany.com/"
+							target="_blank"
+							rel="noreferral"
+						>
+							FreePikCompany
+						</a>
+					</span>
+				</h1>{' '}
+			</div>
+
 			<p>
 				Si usted es una organización sin fines de lucro que desea utilizar esta
 				plantilla y tenerla hosteada de forma gratuita enviar un email a
